c5905233f2fbd0fd1ee972e25bc23ac10f486abc
takutaka <takutaka@pepabo.com>
2023-02-14 23:11:32 +0900

    add testmiddlewere

--- main.go
+++ main.go
@@ -12,12 +12,23 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+func testMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		if os.Getenv("NOT_NEXT") == "true" {
+			return c.String(401, "not next")
+		} else {
+			return next(c)
+		}
+	}
+}
+
 func Start() error {
 	svc := s3.New(session.Must(session.NewSessionWithOptions(session.Options{
 		Profile:           os.Getenv("AWS_PROFILE"),
 		SharedConfigState: session.SharedConfigEnable,
 	})))
 	e := echo.New()
+	e.Use(testMiddleware)
 	e.GET("/", func(c echo.Context) error {
 		input := &s3.ListBucketsInput{}
 		result, err := svc.ListBuckets(input)
@@ -25,6 +36,9 @@ func Start() error {
 			return err
 		}
 		return c.String(http.StatusOK, result.GoString())
+	})
+	e.GET("/hello", func(c echo.Context) error {
+		return c.String(http.StatusOK, "hello")
 	})
 	return e.Start("0.0.0.0:1323")
 }
